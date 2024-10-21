8b199ca0e278eb4ed85b4c2e3947ef8374467d2b
Sarai <saraimontesa@gmail.com>
2024-10-21 12:06:29 +0200

    Strlen con main terminado

--- ft_strlen/ft_strlen.c
+++ ft_strlen/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: sarmonte <sarmonte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 17:21:49 by sarmonte          #+#    #+#             */
-/*   Updated: 2024/10/20 17:32:14 by sarmonte         ###   ########.fr       */
+/*   Updated: 2024/10/21 12:06:09 by sarmonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,31 @@ Your function must be declared as follows:
 
 int	ft_strlen(char *str);
 */
-# include <unistd.h>
 
 int	ft_strlen(char *str)
 {
-    int index;
+	int	index;
 
-    index = 0;
+	index = 0;
 
-    while (str[index])
-        index++;
-    return (index);
+	while (str[index])
+		index++;
+	return (index);
 }
+
+/*
+# include <stdio.h>	// Hace falta para el printf
 
 int main()
 {
-    int len;
-    
-    len = ft_strlen("Hello");
+	int	len;
 
-    write (1, &len, 1);
-    
-    return (0);
+	len = ft_strlen("Hello");
+
+	printf("\n");
+	printf("La cadena proporcionada mide %d caracteres", len);
+	printf("\n\n");
+
+	return (0);
 }
+*/
