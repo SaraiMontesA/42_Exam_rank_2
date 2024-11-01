0547b8c4dee60cedcb3c0785e47f4a1bc7a1cfd6
Sarai Montes <saraimontesa@gmail.com>
2024-11-01 09:44:30 +0100

    Creado ejercicio ft_strcpy.c

--- Ejercicios/Nivel 1/ft_strcpy/ft_strcpy.c
+++ Ejercicios/Nivel 1/ft_strcpy/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: sarmonte <sarmonte@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 17:21:49 by sarmonte          #+#    #+#             */
-/*   Updated: 2024/11/01 09:30:56 by sarmonte         ###   ########.fr       */
+/*   Updated: 2024/11/01 09:42:48 by sarmonte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,43 @@ Your function must be declared as follows:
 char    *ft_strcpy(char *s1, char *s2);
 */
 
-int	ft_strlen(char *str)
+int	ft_strcpy(char *s1, char *s2)
 {
 	int	index;
 
 	index = 0;
 
-	while (str[index])
+	while (s2[index])
+	{
+		s1[index] = s2[index];
 		index++;
+	}
+	s1[index] = "\0";
 	return (index);
 }
 
-/*
 # include <stdio.h>	// Hace falta para el printf
 
 int main()
 {
-	int	len;
-
-	len = ft_strlen("Hello");
+	//int	len;
+	char *s1 = "Cadena 1";
+	char *s2 = "Cadena 2";
 
 	printf("\n");
-	printf("La cadena proporcionada mide %d caracteres", len);
+	printf("Datos originales")
+	printf("Cadena 1: %s", s1);
+	printf("Cadena 2: %s", s2);
+	printf("\n\n");
+	
+	ft_strcpy(s1, s2);
+
+	printf("Datos modificados")
+	printf("Cadena 1: %s", s1);
+	printf("Cadena 2: %s", s2);
 	printf("\n\n");
 
 	return (0);
 }
+/*
 */
